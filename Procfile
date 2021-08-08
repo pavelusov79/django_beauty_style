f3d7ff6271815dec522bb6d1d71a03adff62d09d
pavelusov79 <pavel.usov79@gmail.com>
2021-08-08 21:43:37 +1000

    updates

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn django_beauty_style.wsgi --log-file - 
--- django_beauty_style/settings.py
+++ django_beauty_style/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hrmkh2h1q&4l$=2!8ep^$d!=9p8bx2))zt@r((#8m%lpswel!#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'django_beauty_style.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
